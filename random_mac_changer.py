a590496db5e7da1b70c380f5b587edaa8d7e1762
Bekir Begde <84973367+BegdeBekir@users.noreply.github.com>
2022-12-20 22:21:20 -0500

    Update random_mac_changer.py

--- random_mac_changer.py
+++ random_mac_changer.py
@@ -4,16 +4,19 @@ import subprocess
 import optparse
 import re
 from randmac import RandMac
-
+import time
 
 
 def rand_mac_generator():
     example_mac = "00:00:00:00:00:00"
     generated_mac = RandMac(example_mac)
     return generated_mac
+
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-t", "--times", dest="times",help="How many times will the ip address and mac address change")
+    parser.add_option("-w", "--wait", dest="wait", help="How soon will there be a change address and mac address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info.")
@@ -35,8 +38,8 @@ def get_current_mac(interface):
         print("[-] Could not read MAC address.")
 
 def get_current_ipv4(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig",interface])
-    ip_address_search_result = re.search(r"\d\d\d.\d\d\d.\d\d.\d\d\d",str(ifconfig_result))
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    ip_address_search_result = re.search(r"\d\d\d.\d\d\d.\d\d.\d\d\d", str(ifconfig_result))
 
     if ip_address_search_result:
         return ip_address_search_result.group(0)
@@ -45,19 +48,22 @@ def get_current_ipv4(interface):
 
 
 options = get_arguments()
-current_mac = get_current_mac(options.interface)
 
-print("Current MAC = " + str(current_mac))
+for i in range(0, int(options.times)):
+    current_mac = get_current_mac(options.interface)
+    print("Current MAC = " + str(current_mac))
+    generated_mac = rand_mac_generator()
 
-generated_mac = rand_mac_generator()
+    change_mac(options.interface, str(generated_mac))
 
-change_mac(options.interface,str(generated_mac))
+    current_mac = get_current_mac(options.interface)
+    if current_mac == str(generated_mac):
+        print("[+] MAC address was successfully changed to : " + current_mac)
+    else:
+        print("[-] MAC address did not changed. ")
 
-current_mac = get_current_mac(options.interface)
-if current_mac == str(generated_mac):
-    print("[+] MAC address was successfully changed to : " + current_mac)
-else:
-    print("[-] MAC address did not changed. ")
+    current_ipaddress = get_current_ipv4(options.interface)
+    print("Current IPAddress = " + str(current_ipaddress))
+    print("-------------------WAITING" +" "+str(options.wait)+" "+"Seconds-------------------------------")
+    time.sleep(int(options.wait))
 
-current_ipaddress = get_current_ipv4(options.interface)
-print("Current IPAddress = " + str(current_ipaddress))
